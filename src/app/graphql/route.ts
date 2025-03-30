b300600309c349fd3f5d2e7d0d1e31a561c3ce1d
Mudavath-Giri-Naik <mudavathgirinaik444@gmail.com>
2025-03-30 23:54:55 +0530

    Fixed build error

--- src/app/graphql/route.ts
+++ src/app/graphql/route.ts
@@ -10,12 +10,22 @@ import { NextRequest } from "next/server";
 const schema = await buildSchema({
   resolvers: [MeResolver],
 });
+
 const apolloServer = new ApolloServer({
   schema,
   plugins: [ApolloServerPluginLandingPageLocalDefault()],
   introspection: true,
 });
+
 const handler = startServerAndCreateNextHandler<NextRequest>(apolloServer, {
   context: async (req) => ({ req }),
 });
-export { handler as GET, handler as POST };
+
+// Ensure correct export typing
+export async function GET(req: NextRequest, context: { params: any }) {
+  return handler(req);
+}
+
+export async function POST(req: NextRequest, context: { params: any }) {
+  return handler(req);
+}
